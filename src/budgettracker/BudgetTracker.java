b2052de9dd67e4362391a53d643da9f12812b7c8
armando-n <armando.n@outlook.com>
2015-10-14 23:55:48 -0500

    Screwing around with Design Mode

--- src/budgettracker/BudgetTracker.java
+++ src/budgettracker/BudgetTracker.java
@@ -1,9 +1,15 @@
 package budgettracker;
 
+import javax.swing.JFrame;
+
 public class BudgetTracker {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        JFrame frame = new NewJFrame();
+        
+        frame.pack();
+        frame.setVisible(true);
     }
     
 }
